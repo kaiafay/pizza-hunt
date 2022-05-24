aefc419f696c91646931e5ab2ec298a7f4ac47e1
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-23 19:27:57 -0700

    create the reply API routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { 
+    addComment, 
+    removeComment,
+    addReply,
+    removeReply
+} = require('../../controllers/comment-controller');
 
 // post comment route at /api/comments/:pizzaId
 router.route('/:pizzaId').post(addComment);
 
-// delete comment route at /api/comments/:pizzaId/:commentId
-router.route('/:pizzaId/:commentId').delete(removeComment);
+// PUT reply and DELETE comment at /api/comments/:pizzaId/:commentId
+router
+  .route('/:pizzaId/:commentId')
+  .put(addReply)
+  .delete(removeComment);
+
+// DELETE reply at /api/:pizzaId/:commentId/:replyId
+router.route('/:pizzaId/:commentId/:replyId').delete(removeReply);
 
 module.exports = router;
