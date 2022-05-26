765d8f20a80527bb8808dbdba8da2299263eeb78
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-26 16:08:05 -0700

    add validation to Comment model

--- models/Comment.js
+++ models/Comment.js
@@ -27,10 +27,14 @@ const ReplySchema = new Schema({
 
 const CommentSchema = new Schema({
     writtenBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     commentBody: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
