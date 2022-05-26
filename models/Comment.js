80338c51a6f72b6b549fc1a74232b56f55a3d9e5
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-26 16:09:57 -0700

    add validation to Reply schema

--- models/Comment.js
+++ models/Comment.js
@@ -8,10 +8,14 @@ const ReplySchema = new Schema({
         default: () => new Types.ObjectId()
     },
     replyBody: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     writtenBy: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     createdAt: {
         type: Date,
